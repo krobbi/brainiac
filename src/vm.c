0d271b9f26d274b667e8ce119d5a8ab40639ca8e
krobbi <70552146+krobbi@users.noreply.github.com>
2023-11-08 22:09:51 +0000

    Implement dispatch table

--- src/vm.c
+++ src/vm.c
@@ -7,9 +7,53 @@
 void interpretBytecode(uint8_t *bytecode) {
 #define VM_U8() (*bytecode++)
 #define VM_U16() (bytecode += 2, (uint16_t)((bytecode[-1] << 8) | bytecode[-2]))
+#ifdef __GNUC__
+	__label__
+	vm_OP_HLT,
+	vm_OP_RGT,
+	vm_OP_RGT_U8,
+	vm_OP_RGT_U16,
+	vm_OP_LFT,
+	vm_OP_LFT_U8,
+	vm_OP_LFT_U16,
+	vm_OP_INC,
+	vm_OP_INC_U8,
+	vm_OP_DEC,
+	vm_OP_DEC_U8,
+	vm_OP_OUT,
+	vm_OP_INP,
+	vm_OP_BRZ_U8,
+	vm_OP_BRZ_U16,
+	vm_OP_BNZ_U8,
+	vm_OP_BNZ_U16;
+	
+	static void *dispatchTable[] = {
+		[OP_HLT] = &&vm_OP_HLT,
+		[OP_RGT] = &&vm_OP_RGT,
+		[OP_RGT_U8] = &&vm_OP_RGT_U8,
+		[OP_RGT_U16] = &&vm_OP_RGT_U16,
+		[OP_LFT] = &&vm_OP_LFT,
+		[OP_LFT_U8] = &&vm_OP_LFT_U8,
+		[OP_LFT_U16] = &&vm_OP_LFT_U16,
+		[OP_INC] = &&vm_OP_INC,
+		[OP_INC_U8] = &&vm_OP_INC_U8,
+		[OP_DEC] = &&vm_OP_DEC,
+		[OP_DEC_U8] = &&vm_OP_DEC_U8,
+		[OP_OUT] = &&vm_OP_OUT,
+		[OP_INP] = &&vm_OP_INP,
+		[OP_BRZ_U8] = &&vm_OP_BRZ_U8,
+		[OP_BRZ_U16] = &&vm_OP_BRZ_U16,
+		[OP_BNZ_U8] = &&vm_OP_BNZ_U8,
+		[OP_BNZ_U16] = &&vm_OP_BNZ_U16,
+	};
+#define VM_OP(opcode) vm_##opcode:
+#define VM_DISPATCH() goto *dispatchTable[VM_U8()]
+#define VM_LOOP() VM_DISPATCH();
+#else // __GNUC__
 #define VM_LOOP() for(;;) switch(VM_U8())
 #define VM_OP(opcode) case opcode:
 #define VM_DISPATCH() break
+#endif // __GNUC__
 	uint16_t pointer = 0;
 	uint8_t memory[UINT16_MAX + 1] = {};
 	
@@ -119,9 +163,9 @@ void interpretBytecode(uint8_t *bytecode) {
 			VM_DISPATCH();
 		}
 	}
+#undef VM_LOOP
 #undef VM_DISPATCH
 #undef VM_OP
-#undef VM_LOOP
 #undef VM_U16
 #undef VM_U8
 }
